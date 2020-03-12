a6a0487edd111e427097903ce5f22fa9d19821d4
Gianpiero Macrì <job@netsasa.it>
2020-03-12 17:50:54 +0100

    #806 di nuovo

--- DO.VIVICARE.UI/frmArchivioDocumenti.Designer.cs
+++ DO.VIVICARE.UI/frmArchivioDocumenti.Designer.cs
@@ -164,7 +164,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(712, 591);
+            this.ClientSize = new System.Drawing.Size(712, 597);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnAddNew);
