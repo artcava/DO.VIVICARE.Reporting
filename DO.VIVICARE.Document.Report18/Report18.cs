8b7c06a595987ba4c77c34221c1eb988054981e9
Marco Cavallo <cavallo.marco@gmail.com>
2020-05-21 13:52:30 +0200

    #830 Report 18

---
+++ DO.VIVICARE.Document.Report18/Report18.cs
@@ -0,0 +1,9 @@
+﻿using DO.VIVICARE.Reporter;
+
+namespace DO.VIVICARE.Document.Report18
+{
+    [DocumentReference(FileName = "Report18.xlsx", Name = "Report18", Description = "Una descrizione del file")]
+    public class Report18 : BaseDocument
+    {
+    }
+}
