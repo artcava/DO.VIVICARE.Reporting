daeb65fed7bd4f5b34576047b5f6edd95c9c8092
Marco Cavallo <cavallo.marco@gmail.com>
2020-05-21 13:49:32 +0200

    #832 ASST

---
+++ DO.VIVICARE.Document.ASST/ASST.cs
@@ -0,0 +1,23 @@
+﻿using DO.VIVICARE.Reporter;
+
+namespace DO.VIVICARE.Document.ASST
+{
+    [DocumentReference(FileName = "ASST.xlsx", Name = "ASST", Description = "Elenco delle Aziende Sanitarie")]
+    public class ASST : BaseDocument
+    {
+        [DocumentMemberReference(Column = "B", Position = 1)]
+        public string Description { get; set; }
+
+        [DocumentMemberReference(Column = "C", Position = 2)]
+        public int SAPCode { get; set; }
+
+        [DocumentMemberReference(Column = "D", Position = 3)]
+        public int ATSCode { get; set; }
+
+        [DocumentMemberReference(Column = "E", Position = 4)]
+        public int ASSTCode { get; set; }
+
+        [DocumentMemberReference(Column = "F", Position = 5)]
+        public int IDSintesi { get; set; }
+    }
+}
