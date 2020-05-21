b0b26682507bb9b5788a3a6c3fb5641d175e9243
Marco Cavallo <cavallo.marco@gmail.com>
2020-05-21 13:50:43 +0200

    #833 Comuni

---
+++ DO.VIVICARE.Document.Comuni/Comuni.cs
@@ -0,0 +1,14 @@
+﻿using DO.VIVICARE.Reporter;
+
+namespace DO.VIVICARE.Document.Comuni
+{
+    [DocumentReference(FileName = "Comuni.xlsx", Name = "Comuni", Description = "Elenco dei comuni italiani")]
+    public class Comuni : BaseDocument
+    {
+        [DocumentMemberReference(Column = "A", Position = 0)]
+        public string Name { get; set; }
+
+        [DocumentMemberReference(Column = "B", Position = 1)]
+        public string Code { get; set; }
+    }
+}
