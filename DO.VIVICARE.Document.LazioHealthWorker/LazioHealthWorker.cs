0a0a20332c5383b4f86d8d711859fd5aaccf3d4e
Marco Cavallo <marco.cavallo@webwe.do>
2022-02-21 13:43:41 +0100

    #1355 Task completed

---
+++ DO.VIVICARE.Document.LazioHealthWorker/LazioHealthWorker.cs
@@ -0,0 +1,14 @@
+﻿using DO.VIVICARE.Reporter;
+
+namespace DO.VIVICARE.Document.LazioHealthWorker
+{
+    [DocumentReference(Name = "LazioHealthWorker", Description = "Operatori Sanitari del Lazio", RowStart = 2)]
+    public class LazioHealthWorker: BaseDocument
+    {
+        [DocumentMemberReference(Column = "A", Position = 1, FieldName = "HealthWorker")]
+        public string HealthWorker { get; set; }
+
+        [DocumentMemberReference(Column = "B", Position = 2, FieldName = "WorkType")]
+        public string WorkType { get; set; }
+    }
+}
