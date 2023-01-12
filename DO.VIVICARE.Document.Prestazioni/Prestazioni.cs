817eeb88bf16d75f43e54f2acc93421b23750cec
Marco Cavallo <marco.cavallo@webwe.do>
2023-01-12 13:58:00 +0100

    #1483 Task completed

---
+++ DO.VIVICARE.Document.Prestazioni/Prestazioni.cs
@@ -0,0 +1,17 @@
+﻿using DO.VIVICARE.Reporter;
+
+namespace DO.VIVICARE.Document.Prestazioni
+{
+    [DocumentReference(Name = "Prestazioni", Description = "Elenco delle Prestazioni", RowStart = 2)]
+    public class Prestazioni: BaseDocument
+    {
+        [DocumentMemberReference(Column = "A", Position = 1, FieldName = "HealthService")]
+        public string HealthService { get; set; }
+
+        [DocumentMemberReference(Column = "B", Position = 2, FieldName = "WorkType")]
+        public string WorkType { get; set; }
+
+        [DocumentMemberReference(Column = "C", Position = 2, FieldName = "HealthWorkerType")]
+        public string HealthWorkerType { get; set; }
+    }
+}
